f111ec3310fe187043d9bec83f98bfee61b2666f
tchoforwilson <tchoforwilson@gmail.com>
2023-01-21 00:28:38 +0100

    formatted code

--- controllers/recordController.js
+++ controllers/recordController.js
@@ -4,24 +4,25 @@ import { getAllAppointments } from './appointmentController.js';
 import catchAsync from '../utils/catchAsync.js';
 
 export const createRecord = catchAsync(async (req, res, next) => {
-    // 1. Find if record exist with patient name
-    let patient = await Patient.findOne({ name: req.body.name });
+  // 1. Find if record exist with patient name
+  let patient = await Patient.findOne({ name: req.body.name });
 
-    if (!patient ) {
-        req.body.firstTime = "Yes";
-        patient = await Patient.create(req.body);
-    } else {
-        req.body.firstTime = "No";
-    }
-    // Save appointment
-    req.body.patient = patient.id;
-    const appointment = await Appointment.create(req.body);
+  if (!patient) {
+    req.body.firstTime = 'Yes';
+    patient = await Patient.create(req.body);
+  } else {
+    req.body.firstTime = 'No';
+  }
+  // Save appointment
+  req.body.patient = patient.id;
+  const appointment = await Appointment.create(req.body);
 
-    // send Response
-    res.status(201).json({
-        status: 'success',
-        data: appointment
-    })
-})
+  // send Response
+  res.status(201).json({
+    status: 'success',
+    data: appointment,
+  });
+});
 
-export const getAllRecords =  (req, res, next) => getAllAppointments(req, res, next);
+export const getAllRecords = (req, res, next) =>
+  getAllAppointments(req, res, next);
